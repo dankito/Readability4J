986de4e89879dfe0e139b584adfb059eeb7f9c2d
dankl <dankl@comfylight.com>
2017-12-07 19:15:52 +0100

    Implemented detail tests for sueddeutsche-paradise-papers

--- src/test/kotlin/net/dankito/readability4j/additional/AdditionalReadability4JTests.kt
+++ src/test/kotlin/net/dankito/readability4j/additional/AdditionalReadability4JTests.kt
@@ -1,5 +1,8 @@
 package net.dankito.readability4j.additional
 
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.fail
+import net.dankito.readability4j.Article
 import net.dankito.readability4j.Readability4JTestBase
 import org.junit.Test
 
@@ -29,7 +32,18 @@ open class AdditionalReadability4JTests : Readability4JTestBase() {
 
     @Test
     fun testSueddeutscheParadisePapers() {
-        testPage("https://projekte.sueddeutsche.de/paradisepapers/wirtschaft/carstensen-und-der-pharma-milliardaer-e624335", "sueddeutsche-paradise-papers")
+        val article = testPage("https://projekte.sueddeutsche.de/paradisepapers/wirtschaft/carstensen-und-der-pharma-milliardaer-e624335/", "sueddeutsche-paradise-papers")
+
+        assertEquals("Paradise Papers: Carstensen und der Pharma-Milliardär", article.title)
+        assertStartsWith("Hegen und Pflegen Der CDU-Politiker Peter Harry Carstensen und der Pharma-Unternehmer Frederik Paulsen sind eng miteinander verbandelt. Möglicherweise zu eng Die Eröffnung des neuen, gut 13 Millionen Euro teuren Museums", article.textContent)
+        assertContains("Der frühere Kieler Regierungschef bestreitet den Vorgang im Grunde nicht, nennt aber keine Summen: \"So weit ich", article.textContent)
+        assertContains("Das trifft zwar auf sein Engagement in der Gregor-Mendel-Stiftung zu, die sich um Pflanzenzüchtungen kümmert und deren Kuratoriums-Vorsitzender er ist", article.textContent)
+        assertContains("Den Dokumenten nach hat Carstensen den Posten kaum ein halbes Jahr nach seinem Rücktritt als Ministerpräsident 2012 angenommen, er selbst bezeichnet sich als \"nichtexekutiver Direktor\" des Insel-Museums", article.textContent)
+        assertContains("Alles also nur ein Dienst unter Freunden, nichts dabei? Timo Lange von der Antikorruptionsorganisation Lobbycontrol sieht das ganz anders: \"Das ist schon ein sehr interessanter Fall", article.textContent)
+        assertContains("Immer, wenn Politiker die Seiten wechseln, ob es den Altkanzler Gerhard Schröder (SPD) in die russische Energiewirtschaft zieht, den Kanzleramtsminister Ronald Pofalla (CDU) zur Deutschen Bahn", article.textContent)
+        assertContains("Carstensens Verbindung zu Paulsen, sagt Experte Lange von Lobbycontrol, sei genau so ein Fall, der heute unter die Kieler Karenzzeitregel fallen würde. Lange:", article.textContent)
+        assertContains("Frederik Paulsen lässt jeglichen Verdacht zurückweisen, er würde seinem Freund Carstensen irgendeine \"Entlohnung für politische Gefälligkeiten\" gewähren.", article.textContent)
+        assertContains("Er sei bei dem Weingut wegen seines \"Sachverstands in landwirtschaftlichen Fragen\" beratend tätig. Im Übrigen sei Georgien ein interessantes Land.", article.textContent)
     }
 
     @Test
@@ -37,8 +51,22 @@ open class AdditionalReadability4JTests : Readability4JTestBase() {
         testPage("http://www.zeit.de/2017/48/simbabwes-jugend-robert-mugabe-ruecktritt", "zeit-1")
     }
 
-    protected open fun testPage(url: String, pageName: String) {
-        testPage(url, "additional-test-pages", pageName)
+
+    protected open fun testPage(url: String, pageName: String): Article {
+        return testPage(url, "additional-test-pages", pageName)
+    }
+
+
+    protected open fun assertStartsWith(expected: String, actual: String?) {
+        if(actual?.startsWith(expected) != false) {
+            fail(String.format("Expected \n[%s]\n at start of \n[%s]\n", expected, actual))
+        }
+    }
+
+    protected open fun assertContains(expected: String, actual: String?) {
+        if(actual?.contains(expected) != true) {
+            fail(String.format("Expected \n[%s]\n in \n[%s]\n", expected, actual))
+        }
     }
 
 
