85817765d27b447ad773ee7d4679d7ab7a1906c1
dankl <dankl@comfylight.com>
2017-12-07 21:33:46 +0100

    Integrated Readability commit 9ce4d87: Fall back to the original title if after trimming the text we have too many words before the colon.

--- src/main/kotlin/net/dankito/readability4j/processor/MetadataParser.kt
+++ src/main/kotlin/net/dankito/readability4j/processor/MetadataParser.kt
@@ -105,6 +105,11 @@ open class MetadataParser(protected val regEx: RegExUtil = RegExUtil()): Process
                 if(wordCount(curTitle) < 3) {
                     curTitle = origTitle.substring(origTitle.indexOf(':') + 1)
                 }
+                // But if we have too many words before the colon there's something weird
+                // with the titles and the H tags so let's just use the original title instead
+                else if(wordCount(origTitle.substring(0, origTitle.indexOf(':'))) > 5) {
+                    curTitle = origTitle
+                }
             }
         }
         else if(curTitle.length > 150 || curTitle.length < 15) {
